d8b93eede4ea35083732504a01a0fa6c9ee53d3c
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-03-09 08:54:30 -0400

    Initial commit

---
+++ State Machine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("State Machine | Bradley Morrison");
+            Console.WriteLine();
+
+            Console.ReadKey(true);
+        }
+    }
+}
