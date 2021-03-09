3abc9b65b4f748d9619a065bbad5a8f047f42711
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-03-09 09:41:21 -0400

    created basic script featuring enums and switch statements

--- State Machine/Program.cs
+++ State Machine/Program.cs
@@ -8,13 +8,63 @@ namespace State_Machine
 {
     class Program
     {
+        enum Weapons
+        {
+            pistol,
+            shotgun,
+            smg,
+            ar,
+            sniper
+        }
+
+        static Weapons weapon;
+
         static void Main(string[] args)
         {
-
             Console.WriteLine("State Machine | Bradley Morrison");
             Console.WriteLine();
 
+            Weapons weapon;
+
+
+            // simulated gameplay
+
+            ChangeWeapon(Weapons.ar);
+
+            FireWeapon();
+
             Console.ReadKey(true);
+        }
+
+        static void ChangeWeapon(Weapons newWeapon)
+        {
+            weapon = newWeapon;
+        }
+
+        static void FireWeapon()
+        {
+            switch(weapon)
+            {
+                case Weapons.pistol:
+                    Console.WriteLine("Pistol fired!");
+                    break;
+
+                case Weapons.shotgun:
+                    Console.WriteLine("Shotgun fired!");
+                    break;
+
+                case Weapons.smg:
+                    Console.WriteLine("Submachine Gun fired!");
+                    break;
+
+                case Weapons.ar:
+                    Console.WriteLine("Assault Rifle fired!");
+                    break;
+
+                case Weapons.sniper:
+                    Console.WriteLine("Sniper fired!");
+                    break;
+            }
         }
     }
 }
